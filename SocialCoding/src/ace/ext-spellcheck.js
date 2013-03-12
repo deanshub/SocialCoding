d2f4fb8cdc3507c996405fad5eaa4e87e0c397f2
Dotan Laks <dotanlaks6@gmail.com>
2013-03-12 12:37:33 +0200

    Use Ace-noconflict version

--- SocialCoding/src/ace/ext-spellcheck.js
+++ SocialCoding/src/ace/ext-spellcheck.js
@@ -1,1 +1,43 @@
-ace.define("ace/ext/spellcheck",["require","exports","module"],function(e,t,n){text.spellcheck=!0,host.on("nativecontextmenu",function(e){if(!host.selection.isEmpty())return;var t=host.getCursorPosition(),n=host.session.getWordRange(t.row,t.column),r=host.session.getTextRange(n);host.session.tokenRe.lastIndex=0;if(!host.session.tokenRe.test(r))return;var e=r+" "+PLACEHOLDER;text.value=e,text.setSelectionRange(r.length+1,r.length+1),text.setSelectionRange(0,0),inputHandler=function(t){if(t==e)return"";if(t.lastIndexOf(e)==t.length-e.length)return t.slice(0,-e.length);if(t.indexOf(e)==0)return t.slice(e.length);if(t.slice(-2)==PLACEHOLDER){var r=t.slice(0,-2);if(r.slice(-1)==" ")return r=r.slice(0,-1),host.session.replace(n,r),r}return t}})})
+ace.define('ace/ext/spellcheck', ['require', 'exports', 'module' ], function(require, exports, module) {
+
+
+    text.spellcheck = !false;
+    host.on("nativecontextmenu", function(e){
+        if (!host.selection.isEmpty())
+            return;
+        var c = host.getCursorPosition();
+        var r = host.session.getWordRange(c.row, c.column);
+        var w = host.session.getTextRange(r);
+
+        host.session.tokenRe.lastIndex = 0;
+        if (!host.session.tokenRe.test(w))
+            return;
+        var e = w + " " + PLACEHOLDER;
+        text.value = e;
+        text.setSelectionRange(w.length + 1, w.length + 1);
+        text.setSelectionRange(0, 0);
+
+        inputHandler = function(newVal) {
+            if (newVal == e)
+                return '';
+            if (newVal.lastIndexOf(e) == newVal.length - e.length)
+                return newVal.slice(0, -e.length);
+            if (newVal.indexOf(e) == 0)
+                return newVal.slice(e.length);
+            if (newVal.slice(-2) == PLACEHOLDER) {
+                var val = newVal.slice(0, -2)
+                if (val.slice(-1) == " ") {
+                    val = val.slice(0, -1)
+                    host.session.replace(r, val);
+                    return val;
+                }
+            }
+            
+            return newVal;
+        }
+    });
+
+
+
+});
+

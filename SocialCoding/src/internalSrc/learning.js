25178202d91fe507dad08246b80f5d956218c0df
dean shub <deanshub@gmail.com>
2013-03-13 12:06:36 +0200

    search by url parameter: added

--- SocialCoding/src/internalSrc/learning.js
+++ SocialCoding/src/internalSrc/learning.js
@@ -1,26 +1,34 @@
 jQuery(function(){
 	var searchBox = jQuery('#searchBox');
 	var contentDiv = jQuery('#content');
+	var isFirstLoad = true;
 
 	searchBox.autocomplete({
 		select: function( event, ui ) {
-			
-			var urlName = ui.item.value.replace(' ', '_');
-
-			jQuery.ajax({
-				url : 'http://localhost/socialWiki/index.php/' + urlName,
-				xhrFields: {withCredentials: true},
-				dataType : 'html',
-				success : function( data, textStatus, jqXHR ) {
-					var jqData = jQuery('<div />').html(data);
-					contentDiv.html(jqData.find('#content').html()).append(jqData.find('style'));
-				},
-				error : function( jqXHR, textStatus, errorThrown ) {
-					alert('error');
-				}
-			});
+			showPage(ui.item.value);
 		}
 	});
+
+	function showPage(pageName) {
+		var urlName = pageName.replace(' ', '_');
+
+		jQuery.ajax({
+			url : 'http://localhost/socialWiki/index.php/' + urlName,
+			xhrFields: {withCredentials: true},
+			dataType : 'html',
+			success : function( data, textStatus, jqXHR ) {
+				var jqData = jQuery('<div />').html(data);
+				contentDiv.html(jqData.find('#content').html()).append(jqData.find('style'));
+			},
+			error : function( jqXHR, textStatus, errorThrown ) {
+				alert('error');
+			}
+		});
+	}
+	
+	function changeUrl() {
+		
+	}
 
 	searchBox.keypress(function(){
 		jQuery.ajax({
@@ -29,11 +37,21 @@ jQuery(function(){
 			dataType : 'json',
 			success : function( data, textStatus, jqXHR ) {
 				searchBox.autocomplete('option', 'source', data[1]);
-				searchBox.autocomplete('search');
+				if (!isFirstLoad) {
+					searchBox.autocomplete('search');
+				}
 			},
 			error : function( jqXHR, textStatus, errorThrown ) {
 				alert('error');
 			}
 		});
 	});
+
+	var urlParamSubject = GetURLParameter('Subject');
+	if (urlParamSubject != undefined) {
+		searchBox.val(urlParamSubject);
+		showPage(urlParamSubject);
+	}
+
+	isFirstLoad = false;
 });
--- SocialCoding/src/internalSrc/sc.js
+++ SocialCoding/src/internalSrc/sc.js
@@ -36,3 +36,16 @@ function loadWikiMenues(categories, isMenu){
 	var Categories = res.categorymembers;
 	buildListFromCategories(Categories,isMenu);
 }
+
+function GetURLParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+}

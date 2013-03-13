c976aa3f323c3be48f5a5bc00da4e1723362da0a
Malkiz <omieyal@gmail.com>
2013-03-13 20:00:49 +0200

    added : semi-working module to add projects

---
+++ SocialCoding/src/internalSrc/addProject.js
@@ -0,0 +1,45 @@
+jQuery(function(){
+	var sendButton = jQuery('#sendButton');
+	var repoUrl = jQuery('#repoUrl');
+
+	var userCode = GetURLParameter('code');
+
+	if (userCode != undefined) {
+		//jQuery.support.cors = true;
+		jQuery.ajax({
+			type : 'POST',
+			url : 'https://github.com/login/oauth/access_token?client_id=b611c53472c8da911fa2&client_secret=34127198524aae765d1ed87bd3dd5f5584621188&callback=jsonpCallbackFunc&code=' + userCode,
+			//dataType : 'jsonp',
+			beforeSend: function (request) {
+                request.setRequestHeader("Accept", "application/json");
+            },
+            jsonpCallback : 'jsonpCallbackFunc',
+			xhrFields: {withCredentials: true},
+			success : function( data, textStatus, jqXHR ) {
+				alert(data);
+			},
+			error : function( jqXHR, textStatus, errorThrown ) {
+				alert('error');
+			}
+		});
+	}
+
+	function jsonpCallbackFunc(data) {
+		alert('callback');
+	}
+
+	sendButton.click(function() {
+		jQuery.ajax({
+			type : 'POST',
+			url : 'addProject.php',
+			data : {'repoUrl' : repoUrl.val()},
+			xhrFields: {withCredentials: true},
+			success : function( data, textStatus, jqXHR ) {
+				alert(data);
+			},
+			error : function( jqXHR, textStatus, errorThrown ) {
+				alert('error');
+			}
+		});
+	});
+});

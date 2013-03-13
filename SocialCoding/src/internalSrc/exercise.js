b5a00a953a603bb4dacc13211d031a194f336bbf
Malkiz <omieyal@gmail.com>
2013-03-13 10:21:07 +0200

    garlic to the code

--- SocialCoding/src/internalSrc/exercise.js
+++ SocialCoding/src/internalSrc/exercise.js
@@ -1,7 +1,11 @@
+var editor;
+
 jQuery(document).ready(function(){
+	loadAce();
 	loadIssues();
 	addMenuAnimation();
 	slideClose();
+	activateGarlic();
 });
 
 function loadIssues(){
@@ -23,4 +27,23 @@ function slideClose(){
 
 function slideOpen(){
 	jQuery('.navbox').animate({"left": "+=120px"}, "slow");
+}
+
+function activateGarlic() {
+	jQuery('#userCode').garlic( {
+      	onRetrieve: function ( elem, retrievedValue ) {
+      		editor.setValue(retrievedValue);
+      	}
+  	});
+
+	editor.on('change',function(){
+		jQuery('#userCode').val(editor.getValue());
+		jQuery('#userCode').change();
+	});
+}
+
+function loadAce() {
+	editor = ace.edit("editor");
+	editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/javascript");
 }

2b585756a0f03c6c5d47df29ffb5858e826383da
Malkiz <omieyal@gmail.com>
2013-03-14 09:25:41 +0200

    added add-project link to main page

--- SocialCoding/src/internalSrc/homepage.js
+++ SocialCoding/src/internalSrc/homepage.js
@@ -3,4 +3,12 @@ var isFirstLoad;
 
 jQuery(document).ready(function(){
 	loadWikiMenues("Learned_in_SocialCoding", false);
+
+	initAddProject();
 });
+
+function initAddProject() {
+	jQuery('#addProject').click(function(){
+		window.showModalDialog("addProject.html", null, 'Add Project');
+	});
+}

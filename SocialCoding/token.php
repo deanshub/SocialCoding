55a206ee1bd606e630f66fc68825ddf5f51ff41f
Ariel Jannai <arieljannai@gmail.com>
2013-04-22 11:11:40 +0300

    added options for github
    added login button, add gist

---
+++ SocialCoding/token.php
@@ -0,0 +1,17 @@
+<?php
+
+$data = 'client_id=' . 'b611c53472c8da911fa2' . '&' .
+		'client_secret=' . '34127198524aae765d1ed87bd3dd5f5584621188' . '&' .
+		'code=' . urlencode($_GET['code']);
+
+$ch = curl_init('https://github.com/login/oauth/access_token');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+
+preg_match('/access_token=([0-9a-f]+)/', $response, $out);
+echo $out[1];
+curl_close($ch);
+
+?>
